526d71a9d296f246b411c4f69ad5a3f444826650
qpwr <marcbonsels@gmail.com>
2025-12-05 12:23:10 +0100

    Update plugin.js
    Allow html in tooltips

--- src/plugin.js
+++ src/plugin.js
@@ -5,7 +5,9 @@
 ;(function($) {
 
     // Initialize Tippy tooltips
-    tippy( '[data-tippy-content]' );
+    tippy( '[data-tippy-content]', {
+        allowHTML: true
+    });
 
     // Remove title attribute where tooltip is present
     $( '[data-tippy-content]' ).each( function() {
